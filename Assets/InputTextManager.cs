a4b1c2f59db47a59d470bf19e88178f7722cdcf7
bruce-x-wu <wu.bruce.x@gmail.com>
2023-05-30 11:08:31 -0700

    feat: poc open command line from runtime

--- Assets/InputTextManager.cs
+++ Assets/InputTextManager.cs
@@ -1,26 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
+using System.Diagnostics;
+using System.ComponentModel;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEditor.MPE;
 
 public class InputTextManager : MonoBehaviour
 {
     InputField inputField;
-    GameObject displayTextObject;
     DisplayTextManager displayTextManager;
 
     private void Awake()
     {
-        this.displayTextObject = GameObject.FindGameObjectWithTag("DisplayText");
-        this.displayTextManager = this.displayTextObject.GetComponent<DisplayTextManager>();
+        this.displayTextManager = GameObject.FindGameObjectWithTag("DisplayText").GetComponent<DisplayTextManager>();
         this.inputField = this.GetComponent<InputField>();
         this.inputField.onEndEdit.AddListener(this.AcceptStringInput);
     }
 
     void AcceptStringInput(string userInput)
     {
-        displayTextManager.PrintStringToDisplay(userInput);
+        displayTextManager.PrintStringToDisplay("Working on the process. Input Text: [" + this.inputField.text + "]");
+
+        string output = this.ExecuteProcessTerminal("ls");
+
+        UnityEngine.Debug.Log(output);
+        displayTextManager.PrintStringToDisplay("Valid and worked. Output: [" + output + "]");
         this.inputField.text = "";
         this.inputField.ActivateInputField();
+    }
+
+    private string ExecuteProcessTerminal(string argument)
+    {
+        try
+        {
+            UnityEngine.Debug.Log("============== Start Executing [" + argument + "] ===============");
+            ProcessStartInfo startInfo = new ProcessStartInfo()
+            {
+                FileName = "C:\\Program Files\\Git\\git-cmd.exe",
+                UseShellExecute = false,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                // CreateNoWindow = true,
+                Arguments = " -c \"" + argument + " \""
+            };
+            Process myProcess = new Process
+            {
+                StartInfo = startInfo
+            };
+            myProcess.Start();
+            UnityEngine.Debug.Log("Right after start");
+            string output = myProcess.StandardOutput.ReadToEnd();
+            myProcess.WaitForExit();
+            UnityEngine.Debug.Log("============== End ===============");
+
+            return output;
+        }
+        catch (Exception e)
+        {
+            UnityEngine.Debug.LogError(e);
+            return null;
+        }
     }
 }
